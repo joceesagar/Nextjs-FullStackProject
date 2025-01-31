b1887e472bde84bcc14f76640d633597ea751316
joceesagar <joshisagarm2@gmail.com>
2025-01-31 20:55:24 +0545

    Sucessfully implemented custom credentials using next-auth

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,8 +1,27 @@
 import "next-auth"
+import { DefaultSession } from "next-auth"
 
 //next-auth by default doesnot provide much in user object so we add the necessary field required for our app. For example without this code we cannot do user.id because next-auth user doesnot return much data
 declare module "next-auth" {
     interface User {
+        _id?: String
+        isVerified?: boolean
+        isAcceptingMessages?: boolean
+        username?: string
+    }
+    interface Session {
+        user: {
+            _id?: String
+            isVerified?: boolean
+            isAcceptingMessages?: boolean
+            username?: string
+        } & DefaultSession['user']
+    }
+}
+
+//another way of changing interface
+declare module "next-auth/jwt" {
+    interface JWT {
         _id?: String
         isVerified?: boolean
         isAcceptingMessages?: boolean
